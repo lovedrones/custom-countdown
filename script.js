eec8eaef6e44e3c27f5b4914303ac23160f89d3a
Juan Lo <lovedrones@gmail.com>
2022-09-16 19:37:20 -0400

    local storage

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ const countdownBtn = document.getElementById('countdown-button');
 const timeElements = document.querySelectorAll('span');
 
 const completeEl = document.getElementById('complete');
-const completeElInfo = document.getElementById('complete-info');
+const completeElInfo = document.getElementsByClassName('complete-info');
 const completeBtn = document.getElementById('complete-button');
 
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
 let countdownActive; 
+let savedCountdown;
 
 const second = 1000;
 const minute = second * 60;
@@ -31,8 +32,6 @@ function updateDOM() {
     countdownActive = setInterval(() => {
     const now =  new Date().getTime();
     const distance = countdownValue - now;
-    console.log('distance', distance);
-
     const days = Math.floor(distance / day);
     const hours = Math.floor((distance % day ) / hour);
     const minutes = Math.floor((distance % hour) / minute);
@@ -65,14 +64,17 @@ function updateCountdown(e) {
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value;
-    console.log(countdownTitle, countdownDate);
+   savedCountdown = {
+       title: countdownTitle,
+       date: countdownDate,
+   };
+   localStorage.setItem('countdown', JSON.stringify(savedCountdown));
     // Check for valid date
     if (countdownDate === '') {
         alert('Please select a date for the countdown.');
     } else {
         //Get number version of current Dat , updateDOM
         countdownValue = new Date(countdownDate).getTime();
-        console.log('countdown value:',countdownValue); 
         updateDOM();
     }
 }
@@ -89,8 +91,25 @@ function reset() {
     //Reset values
     countdownTitle = '';
     countdownDate = ''; 
+    localStorage.removeItem('countdown');
 }
+
+function restorePreviousCountdown() {
+    // Get countdown from localStorage
+    if (localStorage.getItem('countdown')) {
+        inputContainer.hidden = false; 
+        savedCountdown = JSON.parse(localStorage.getItem('countdown'));
+        countdownTitle = savedCountdown.title;
+        countdownDate = savedCountdown.date;
+        countdownValue = new Date(countdownDate).getTime();
+        updateDom();
+    }
+}
+
 // Event Listeners 
 countdownForm.addEventListener('submit', updateCountdown);
 countdownBtn.addEventListener('click', reset);
 completeBtn.addEventListener('click', reset);
+
+// On Load, check localStorage 
+restorePreviousCountdown();
